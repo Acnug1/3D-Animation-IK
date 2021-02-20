3dbcf44aa50f7d61583ccc8cb65c1b6c537b27ad
Acnug1 <59884220+Acnug1@users.noreply.github.com>
2021-02-20 14:55:59 +0500

    Init

---
+++ Scripts/LookAtIK.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+[RequireComponent(typeof(Animator))]
+public class LookAtIK : MonoBehaviour
+{
+    protected Animator animator;
+
+    public bool IkActive = false;
+    public Transform LookObj = null;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void OnAnimatorIK()
+    {
+        if (animator)
+        {
+            if (IkActive)
+            {
+                if (LookObj != null)
+                {
+                    animator.SetLookAtWeight(1);
+                    animator.SetLookAtPosition(LookObj.position);
+                }
+            }
+            else
+            {
+                animator.SetLookAtWeight(0);
+            }
+        }
+    }
+}
